cf7b13cd5aa8c1f8cdcc563754d9d6270b7979f7
Uliana <a_k_ula@ukr.net>
2022-11-28 05:36:12 +0200

    working game

---
+++ ApplePicker/Assets/Scripts/Apple.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static float bottomY = -20f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < bottomY)
+        {
+            Destroy(gameObject);
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+        }
+    }
+}
---
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasket = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasket.transform.position = pos;
+            basketList.Add(tBasket);
+        }
+
+        
+    }
+    public void AppleDestroyed()
+    {
+        GameObject[] delList = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject apple in delList)
+        {
+            Destroy(apple);
+        }
+        int basketindex = basketList.Count - 1;
+        GameObject basket = basketList[basketindex];
+        basketList.RemoveAt(basketindex);
+        Destroy(basket);
+
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene_1");
+        }
+    }
+
+}
